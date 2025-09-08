0a73e89fa4785e0ab292d21c123046015984b507
muhammad-shazaib <shazaibirshad126@gamil.com>
2025-09-08 22:08:25 +0500

    variables declarations details explore  const, let, var, console.log/table and terminal

---
+++ 02_variables.js
@@ -0,0 +1,14 @@
+
+const accountId = 144553;
+let accountEmail = "kzZVb@example.com";
+var accountPassword = "12345";
+accountCity = "Karachi";
+let accountState;
+
+//accountId = 123456; // not allowed
+accountEmail = "G2EYl@example.com";
+accountPassword = "123456";
+accountCity = "Lahore";
+
+console.log(accountId, accountEmail, accountPassword, accountCity, accountState);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
