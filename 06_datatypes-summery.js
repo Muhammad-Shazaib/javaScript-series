a94d5ad82062df56b24620d2aaf121c9cdf2a951
muhammad-shazaib <shazaibirshad126@gamil.com>
2025-09-09 22:18:11 +0500

    dataType summery explore Primitive & non-primitive

---
+++ 06_datatypes-summery.js
@@ -0,0 +1,28 @@
+// Data types in JavaScript
+// There are 7 primitive data types in JavaScript
+// 1. String "" or ''
+// 2. Number 0 or 1 and power of 53
+// 3. Boolean true or false
+// 4. null (when variable is empty)
+// 5. undefined (when variable is not assigned any value)
+// 6. BigInt (2n, 3n)
+// 7. Symbol (used in advanced programming)
+
+console.log(typeof "Hello") // string
+console.log(typeof 42) // number
+console.log(typeof true) // boolean true or false
+console.log(typeof null) // empty value // 
+console.log(typeof undefined) // not assigned value //
+console.log(typeof Symbol()) // unique value ka liye used hota hai symbol
+
+// Non-primitive (Reference) data type
+
+// 1. Object (used to store multiple values in a single variable)
+// 2. Array (used to store multiple values in a single variable in a list form)
+// 3. Function (used to perform a task)
+
+console.log(typeof { name: "Shazaib", age: 31 }) // object
+console.log(typeof [1, 2, 3, 4, 5]) // object (array is a type of object)
+console.log(typeof function() {}) // function (used to perform a task)
+
+
