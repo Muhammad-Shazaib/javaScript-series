be823e66a268a8e0b86f0f38d276f1fa70e022a8
muhammad-shazaib <shazaibirshad126@gamil.com>
2025-09-10 19:32:50 +0500

    comment out example code for array methods and add clarity to slice and splice usage

--- 10_arrays.js
+++ 10_arrays.js
@@ -2,16 +2,39 @@
 // An array is a special variable that can hold more than one value at a time.
 // Arrays are used to store a collection of items in a single variable.
 
-const fruits = ["apple", "banana", "cherry"];
-console.log(fruits); // [Array: 3 items]
-console.log(fruits.length); // [Number: 3]
-console.log(fruits[0]); // [String: "apple"]
-console.log(fruits[1]); // [String: "banana"]
-console.log(fruits[2]); // [String: "cherry"]
+// const fruits = ["apple", "banana", "cherry"];
+// console.log(fruits); // [Array: 3 items]
+// console.log(fruits.length); // [Number: 3]
+// console.log(fruits[0]); // [String: "apple"]
+// console.log(fruits[1]); // [String: "banana"]
+// console.log(fruits[2]); // [String: "cherry"]
 
 // Array methods
-//fruits.push("date"); // add "date" to the end
-//fruits.push("mango"); // add "mango" to the end
-//fruits.pop(); // remove last item ("mango")
+// fruits.push("date"); // add "date" to the end
+// fruits.push("mango"); // add "mango" to the end
+// fruits.pop(); // remove last item ("mango")
 
-console.log(fruits); // [Array: 5 items]
+// fruits.unshift("kiwi"); // add "kiwi" to the 0 index
+// fruits.shift(); // remove first item ("kiwi")
+
+// console.log(fruits.indexOf("banana")); // [Number: 1]
+// console.log(fruits.includes("cherry")); // [Boolean: true]
+
+// const newFruits = fruits.join();
+
+// console.log(fruits); // [Array: 4 items]
+// console.log(typeof newFruits); // [String: "apple,banana,cherry,date"]
+
+// slice() method
+// splice() method
+const arr1 = [1, 2, 3, 4, 5];
+console.log("A", arr1.slice(1, 3)); // [Array: 2 items]
+
+const myn1 = arr1.slice(1, 3);
+
+console.log(arr1); // [Array: 5 items]
+console.log("B", myn1); // [Array: 2 items]
+
+const myn2 = arr1.splice(1, 3);
+console.log(arr1); // [Array: 2 items]
+console.log("C", myn2); // [Array: 3 items]
