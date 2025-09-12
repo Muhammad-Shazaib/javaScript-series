f0b0780096ec8d09c7752b53f9cea619a31ac7f1
muhammad-shazaib <shazaibirshad126@gamil.com>
2025-09-12 21:47:16 +0500

    enhance comments for slice and splice methods to clarify their behavior

--- 10_arrays.js
+++ 10_arrays.js
@@ -28,13 +28,13 @@
 // slice() method
 // splice() method
 const arr1 = [1, 2, 3, 4, 5];
-console.log("A", arr1.slice(1, 3)); // [Array: 2 items]
+console.log("A", arr1.slice(1, 3)); // [Array: 2 items] // [2, 3] // does not modify the original array
 
-const myn1 = arr1.slice(1, 3);
+const myn1 = arr1.slice(1, 3); // [2, 3] // does not modify the original array 
 
-console.log(arr1); // [Array: 5 items]
+console.log(arr1); // [Array: 5 items] 
 console.log("B", myn1); // [Array: 2 items]
 
-const myn2 = arr1.splice(1, 3);
-console.log(arr1); // [Array: 2 items]
-console.log("C", myn2); // [Array: 3 items]
+const myn2 = arr1.splice(1, 3); // [2, 3, 4] // modifies the original array
+console.log(arr1); // [Array: 2 items] // [1, 5]
+console.log("C", myn2); // [Array: 3 items] // [2, 3, 4]
